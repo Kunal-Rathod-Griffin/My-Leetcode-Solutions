d2471fca4ddf1348163ff52451cf375142312d58
Kunal Rathod <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2022-07-03 18:16:50 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (71.88%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,73 +1,16 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
         
-//         int n = nums.length - 1;
+        //using dynamic programming
         
-//         if(n == 0){
-//             return n+1;
-//         }
+        if(nums.length < 2) return nums.length;
         
-//         int[] diff = new int[n-1];
+        int down  = 1, up  = 1;
         
-//         for(int i =0; i<diff.length; i++){
-//             diff[i] = nums[i+1] - nums[i];
-//         }
-        
-//         int ans = nums.length;
-        
-//         Boolean is_positive = null;
-        
-//         for(int i =0; i<diff.length; i++){
-            
-//             if(diff[i] == 0)
-//             {
-//                 ans--;
-                
-//             }
-//             else if(is_positive == null){
-//                 is_positive = diff[i] > 0;
-                 
-//             }
-            
-//             else if((diff[i]>0 && is_positive) || (diff[i]<0 && !is_positive)){
-//                 ans--;
-                 
-//             }
-//             else{
-//                 System.out.println(diff[i] + " here");
-//                 is_positive = !is_positive;
-//             }
-            
-            
-//         }
-        
-//         for(int i =0;i<diff.length; i++){
-//             System.out.println(diff[i]);
-//         }
-        
-//         return ans;
-    if (nums.length <= 1) return nums.length;
-    
-    int count = nums.length;
-    Boolean positive = null;
-    
-    for (int i = 0; i < nums.length-1; i++){
-        int temp = nums[i+1] - nums[i];
-        if (temp == 0) count--;
-        else if (positive == null) positive = temp > 0;
-        else if ((temp > 0 && positive) || (temp < 0 && !positive))
-        {    
-            System.out.println(i);
-            count--;
+        for(int i = 1; i< nums.length; i++){
+            if(nums[i] > nums[i-1]) up = down + 1;
+            else if(nums[i] < nums[i-1]) down = up + 1;
         }
-        else{
-            positive = !positive;
-            
-        }
-    }
-    return count;
-
-        
-        
+        return Math.max(down, up);
     }
 }
