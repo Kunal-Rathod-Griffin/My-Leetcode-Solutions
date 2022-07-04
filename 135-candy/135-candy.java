e9c2e6f69b56d8b11f96e36ac11e35dfe3103cc6
Kunal Rathod <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2022-07-04 23:38:01 +0530

    Time: 4 ms (70.03%), Space: 51.4 MB (71.36%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,17 +1,30 @@
-// class Solution {
-//     public int candy(int[] ratings) {
+class Solution {
+    public int candy(int[] ratings) {
+            
+        int[] candies = new int[ratings.length];
+        Arrays.fill(candies, 1);
         
+        for(int i =0; i<ratings.length - 1; i++){
+            if(ratings[i+1] > ratings[i]) candies[i+1] = candies[i] + 1;
+        }
+        for(int i = ratings.length -2; i>=0; i--){
+            if(ratings[i] > ratings[i+1] && candies[i] <=candies[i+1]) candies[i] = candies[i+1] +  1;
+         }
         
-        
+        int sum = 0;
+        for(int s : candies){
+            sum+=s;
+        }
+        return sum;
 //         int n = ratings.length;
 //         int[] candies = new int[n];
         
 //         Arrays.fill(candies, 1);
         
-//         boolean changed = false;
+//         boolean changed = true;
         
-//         while(true){
-            
+//         while(changed){
+//             changed = false;
 //             for(int i =0; i<n; i++){
 //                 if(i!=n-1 &&  ratings[i+1] < ratings[i] && candies[i+1] >= candies[i]){
 //                     changed = true;
@@ -23,14 +36,6 @@
 //                 }
                 
 //             }
-            
-//             if(changed == true){
-//                 break;
-//             }
-//             else{
-//                 changed = false;
-//             }
-            
 //         }
         
 //         int sum = 0;
@@ -39,31 +44,31 @@
 //         }
 //         return sum;
         
-//     }
-// }
-
-public class Solution {
-    public int candy(int[] ratings) {
-        int[] candies = new int[ratings.length];
-        Arrays.fill(candies, 1);
-        boolean hasChanged = true;
-        while (hasChanged) {
-            hasChanged = false;
-            for (int i = 0; i < ratings.length; i++) {
-                if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
-                    candies[i] = candies[i + 1] + 1;
-                    hasChanged = true;
-                }
-                if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
-                    candies[i] = candies[i - 1] + 1;
-                    hasChanged = true;
-                }
-            }
-        }
-        int sum = 0;
-        for (int candy : candies) {
-            sum += candy;
-        }
-        return sum;
     }
 }
+
+// public class Solution {
+//     public int candy(int[] ratings) {
+//         int[] candies = new int[ratings.length];
+//         Arrays.fill(candies, 1);
+//         boolean hasChanged = true;
+//         while (hasChanged) {
+//             hasChanged = false;
+//             for (int i = 0; i < ratings.length; i++) {
+//                 if (i != ratings.length - 1 && ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+//                     candies[i] = candies[i + 1] + 1;
+//                     hasChanged = true;
+//                 }
+//                 if (i > 0 && ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+//                     candies[i] = candies[i - 1] + 1;
+//                     hasChanged = true;
+//                 }
+//             }
+//         }
+//         int sum = 0;
+//         for (int candy : candies) {
+//             sum += candy;
+//         }
+//         return sum;
+//     }
+// }
