0213fbcfa9664a326e5d866d1a128c7583b298ba
Kunal Rathod <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2022-07-06 12:37:08 +0530

    Time: 7 ms (72.56%), Space: 59.2 MB (17.81%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,76 +1,41 @@
-// class Solution {
-//     public int maximalSquare(char[][] matrix) {
-        
-//         int rows = matrix.length;
-//         int cols = rows > 0 ? matrix[0].length : 0;
-        
-//         int max_len = 0;
-        
-//         for(int i =0; i<rows; i++){
-//             for(int j =0; j<cols; j++){
-                
-//                 if(matrix[i][j] == '1'){
-//                     int sqlen = 1;
-//                     boolean flag = true;
-                    
-//                     while(sqlen + i < rows && sqlen + j < cols && flag){
-                        
-//                         for(int k  =j; k<= sqlen; k++){
-//                             if(matrix[i + sqlen][k] == '0'){
-//                                 flag = false;
-//                                 break;
-//                             }
-//                         }
-                        
-//                         for(int k = i; k <=sqlen; k++){
-//                             if(matrix[k][j + sqlen] == '0'){
-//                                 flag = false;
-//                                 break;
-//                             }
-//                         }
-//                         if(flag) sqlen++;
-//                     }
-                    
-//                     if(max_len < sqlen)
-//                         max_len = sqlen;
-//                 }
-//             }
-//         }
-//         return max_len * max_len;
-//     }
-// }
+/*
+Using dynamic programming for this approc
 
-public class Solution {
+if there is 
+1 1 
+1 1 we will make it 1 1 
+                    1 2 so now if there is another 1s like
+
+1 1 0
+1 2 1
+1 1 1 we will make it to 
+
+1 1 0
+1 2 1
+1 1 2 becuase we are doing min of i-1,j;  i, j-1; i-1, j-1;
+
+*/
+
+
+class Solution {
     public int maximalSquare(char[][] matrix) {
+        
         int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
+        int[][] dp = new int[rows + 1][cols + 1];
         int maxsqlen = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == '1') {
-                    int sqlen = 1;
-                    boolean flag = true;
-                    while (sqlen + i < rows && sqlen + j < cols && flag) {
-                        for (int k = j; k <= sqlen + j; k++) {
-                            if (matrix[i + sqlen][k] == '0') {
-                                flag = false;
-                                break;
-                            }
-                        }
-                        for (int k = i; k <= sqlen + i; k++) {
-                            if (matrix[k][j + sqlen] == '0') {
-                                flag = false;
-                                break;
-                            }
-                        }
-                        if (flag)
-                            sqlen++;
-                    }
-                    if (maxsqlen < sqlen) {
-                        maxsqlen = sqlen;
-                    }
+        
+        for(int i = 1; i<= rows; i++){
+            for(int j = 1; j<= cols; j++){
+                if(matrix[i-1][j-1] == '1'){
+                    dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
+                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
                 }
             }
         }
-        return maxsqlen * maxsqlen;
+        
+        
+        
+        
+        return maxsqlen*maxsqlen;
     }
 }
