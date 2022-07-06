49053b5151a8855bc0b1e9ae026693184057b203
Kunal Rathod <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2022-07-06 11:20:28 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (75.12%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
         
-        return fib(n-1) + fib(n-2);
+        if(n<=1) return n;
+        
+        int prev = 0;
+        int curr = 1;
+        
+        while(n>1){
+            int sum = curr+prev;
+            prev = curr;
+            curr=sum;
+            n--;
+        }
+        
+        return curr;
     }
 }
